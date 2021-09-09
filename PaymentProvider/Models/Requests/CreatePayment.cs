a86909ac86d1bc53c6ab385f4744ff82068f4c9f
Artashes Ter-Abrahamyan <artashes@7smarts.com>
2021-09-09 13:28:14 +0400

    Add project files.

---
+++ PaymentProvider/Models/Requests/CreatePayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentProvider.Models.Requests
+{
+    public class CreatePayment
+    {
+        public string OrderId { get; set; }
+        public string Number { get; set; }
+        public string Currency { get; set; }
+        public string Country { get; set; }
+        public string CardNumber { get; set; }
+        public string CardHolder { get; set; }
+        public string CardExpiryDate { get; set; }
+        public string Cvv { get; set; }
+    }
+}
